9c0812f808dc59897b7a4d972a18d8703a781791
Erik Aung <aungnaingthu@firstcom.com.sg>
2022-01-26 11:58:26 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, {useEffect, useState} from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 function App() {
+  
+  //fetch api
+  const [users,setUsers] = useState([]);
+
+  const fetchData = () => {
+    fetch("http://jsonplaceholder.typicode.com/users")
+    .then(response => {
+      return response.json()
+     
+    })
+    .then(data => {
+      setUsers(data)
+    })
+  }
+  useEffect(() => {
+    fetchData()
+  },[])
+
+  //react slick
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.length > 0 && (
+        <div className='flex flex-col m-10'> 
+          <Slider {...settings}>
+          {users.map(user => (  
+            
+            <div className="text-center text-sky-400 flex flex-col" key={user.id}>
+              <p> {user.name} </p>
+              <p> {user.email} </p>
+            </div>  
+            
+          ))}
+          </Slider>
+          
+        </div>
+      )}
     </div>
   );
 }
